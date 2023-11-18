4549a3c2323193096e42d66dc728f31bbe31e62e
Evgeniy2022 <t0lrk0ln22rus@mail.ru>
2023-11-18 13:09:07 +0300

    three commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 
 let iconMenu = document.querySelector('.navig-menu__icon')
-const menuBody = document.querySelector('.navig');
+let menuBody = document.querySelector('.navig');
+let mainLang = document.querySelector('.main-name__lang');
 if(iconMenu){
 	iconMenu.addEventListener("click", function(e){
 		document.body.classList.toggle('_lock')
 		iconMenu.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
+		mainLang.classList.toggle('_active');
 	})
 }
 
@@ -13,26 +15,26 @@ if(iconMenu){
 
 let menuLinks = document.querySelectorAll('.menu-link[data-goto]');
 
-if(menuLinks.length > 0){
+function menuLinkCl(){
 	menuLinks.forEach(menuLink =>{
 		menuLink.addEventListener("click", oneMenuLinkClick);
 })
 	function oneMenuLinkClick (e){
 		const menuLink = e.target;
-		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
-			const gotoBlock = document.querySelector(menuLink.dataset.goto);
-			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + document.querySelector('header').offsetHeight;
+		const gotoBlock = document.querySelector(menuLink.dataset.goto);
+		const gotoBlockValue = gotoBlock.getBoundingClientRect().top + document.querySelector('header').offsetHeight;
 
-			if (iconMenu.classList.contains('_active')){
-				document.body.classList.remove('_lock')
-				iconMenu.classList.remove('_active');
-				menuBody.classList.remove('_active');
-			}
-
-			window.scrollTo({
-				top: gotoBlockValue,
-				behavior: "smooth"
-			});
+		if (iconMenu.classList.contains('_active')){
+			document.body.classList.remove('_lock')
+			iconMenu.classList.remove('_active');
+			menuBody.classList.remove('_active');
+			mainLang.classList.remove('_active');
 		}
+
+		window.scrollTo({
+			top: gotoBlockValue,
+			behavior: "smooth"
+		});
 	}
 }
+menuLinkCl()
